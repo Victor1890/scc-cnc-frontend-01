348592dac9b50ed1a1775a7ac5e9255140fc9fc8
Victor Jesús Rosario Vásquez <46900196+Victor1890@users.noreply.github.com>
2023-12-20 14:12:42 -0400

    Update perimeterBackground field in biomedical modal

--- components/pages/request/modal/biomedical/fields/perimeter-background.ts
+++ components/pages/request/modal/biomedical/fields/perimeter-background.ts
@@ -4,7 +4,19 @@ export const perimeterBackground: FieldConfig<any>[] = [
     {
         label: "Edad gestacional",
         name: "ageGestional",
-        type: "number",
+        type: "select",
+        options: [
+            "Semana 1-2",
+            "Semana 3-8",
+            "Semana 9-12",
+            "Semana 13-16",
+            "Semana 17-20",
+            "Semana 21-24",
+            "Semana 25-28",
+            "Semana 29-32",
+            "Semana 33-36",
+            "Semana 37-40",
+        ].map((label) => ({ label, value: label })),
         responsive: {
             xs: 3
         },
@@ -56,7 +68,6 @@ export const perimeterBackground: FieldConfig<any>[] = [
         name: "perinatalDiseaseHistoryUUIDs",
         type: "multi-select",
         options: [
-            "Reanimación",
             "Asfixia al nacer",
             "Distrés Respiratorio",
             "Sepsis neonatal",
